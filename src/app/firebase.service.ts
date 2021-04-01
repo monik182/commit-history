e3e1249f9ca8aee6e674611f808f50a5eb5c2b1d
Monica Carvajal <mcarvajal@leal.co>
2021-04-01 12:50:49 -0500

    Se instala firebase

---
+++ src/app/firebase.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import firebase from 'firebase/app';
+import { firebaseConfig } from '../firebase-config';
+import 'firebase/remote-config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+  remoteConfig;
+  constructor() {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  async fetch(key: string) {
+    await this.fetchAndActivate();
+    return this.remoteConfig.getString(key);
+  }
+
+  private async fetchAndActivate() {
+    if (!this.remoteConfig) {
+      this.remoteConfig = firebase.remoteConfig();
+      this.remoteConfig.settings.minimumFetchIntervalMillis = 60000;
+    }
+    await this.remoteConfig.fetchAndActivate();
+  }
+}
---
+++ src/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyD-EZ7se73djS7hNu6R_yGPSHAumqBLyUY',
+  authDomain: 'commit-history.firebaseapp.com',
+  projectId: 'commit-history',
+  storageBucket: 'commit-history.appspot.com',
+  messagingSenderId: '1087685459329',
+  appId: '1:1087685459329:web:8cbf957c2256dc2f373c80',
+  measurementId: 'G-VJ3GWS2L6M'
+};
